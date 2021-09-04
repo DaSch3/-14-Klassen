ebcda6b6e53dcdc6c94740a95717cbd23aa5b63a
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:33:15 +0200

    Projektdateien hinzufügen.

---
+++ Klassen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person janek = new Person();
+            Person hendrik = new Person();
+
+            janek.Name = "Janek";
+            janek.SageHallo();
+
+            hendrik.Name = "Hendrik";
+            hendrik.SageHallo();
+
+            Console.ReadKey();
+        
+        }
+    }
+
+    class Person
+    {
+        //Eigenschaften
+        public string Name { get; set; }
+        public int Alter { get; set; }
+
+        //Methoden
+        public void SageHallo()
+        {
+            Console.WriteLine(Name + " sagt: Hallo!");
+        }
+    }
+}
